b534818cf9a3725aed8b8ea4416d7d62d55e6716
badon1933 <badon1933@gmail.com>
2023-12-22 13:32:20 +0700

    Hari 1 Sesi 1

---
+++ string/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Badon")
+	fmt.Println("Badon Ganteng")
+	fmt.Println("Badon Ganteng Banget")
+
+	fmt.Println(len("Badon"))
+	fmt.Println("Badon Ganteng"[0])
+	fmt.Println("Badon Ganteng Banget"[3])
+}
